6f4ba6925cac9857321c6f43ac2e673124f296b7
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-02-23 22:03:27 +0900

    Update OrderCar.java

--- main/java/mini_project/car_sales_company/domain/OrderCar.java
+++ main/java/mini_project/car_sales_company/domain/OrderCar.java
@@ -12,7 +12,9 @@ public class OrderCar {
     @Id @GeneratedValue
     @Column(name = "order_car_id")
     private Long id;
-
+    
+    //LAZY로 설정해서 처음에는 프록시객체로 받아서 영속성 컨텍스트에 저장하고
+    //만약 이 데이터에 접근 할 때만 DB에 Select Query를 날려서 영속석 컨택스트에 저장한다.
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "car_id")
     private Car car;
